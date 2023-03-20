0644f38227db33a08dec4647917311bbaf454dfe
Sam <boostensam@gmail.com>
2023-03-20 14:47:03 +0100

    overzetten naar github

---
+++ app/public/wp-content/uploads/mailchimp-for-wp/debug-log.php
@@ -0,0 +1,3 @@
+<?php exit; ?>
+[2022-05-01 12:49:59] WARNING: Form 238598 > test***@gm***.com is already subscribed to the selected list(s)
+[2022-05-01 12:49:59] WARNING: Form 238598 > test***@gm***.com is already subscribed to the selected list(s)
